7498515547119c0b8e9d988ea5d6d8d81c9d47a3
theoutreachprojectpodcast-ui <theoutreachprojectpodcast@gmail.com>
2026-07-25 16:27:22 -0700

    Register Google Play external content links plugin

--- web/android/app/src/main/java/org/theoutreachproject/top/MainActivity.java
+++ web/android/app/src/main/java/org/theoutreachproject/top/MainActivity.java
@@ -8,6 +8,7 @@ import com.getcapacitor.BridgeActivity;
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        registerPlugin(GooglePlayExternalContentLinksPlugin.class);
         lockPortrait();
         super.onCreate(savedInstanceState);
     }
